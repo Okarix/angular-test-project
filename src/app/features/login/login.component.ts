7720fa1bce5a4d2052a7454ccd1d15ef3dbf7469
Okarix <datkaevadiar@gmail.com>
2025-01-28 00:02:49 +0500

    feat: login page

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  form = new FormGroup({
+    username: new FormControl(null),
+    password: new FormControl(null),
+  });
 
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
